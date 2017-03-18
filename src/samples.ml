4c2983210745f6beab46dc022d57fb0970b379d3
zw <wzhao1984@gmail.com>
2017-03-19 00:36:24 +0800

    optimized todo sample

--- src/samples.ml
+++ src/samples.ml
@@ -2,4 +2,6 @@ let counter = let open Counter in main
 let field = let open Field in main
 let form = let open Form in main
 let todo = let open Todo in main
+let todo_optimized = let open Todo_optimized in main
+
 
